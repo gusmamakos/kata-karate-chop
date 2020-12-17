453dd2229d97a025e4bc99bc313a60815c8c9351
Constantine Mamakos <mamakos@andrew.cmu.edu>
2020-12-17 16:23:14 -0500

    Updated to parameterized testing

--- src/test/java/com/mamakos/ChopTest.java
+++ src/test/java/com/mamakos/ChopTest.java
@@ -1,56 +1,62 @@
 package com.mamakos;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(value= Parameterized.class)
 public class ChopTest {
-    @Test
-    public void testChop() {
-        IChop c = new Chop1();
-        assert -1 == c.chop(3, new int[]{}) : "Test 1: Failed";
-        assert -1 == c.chop(3, new int[]{1}) : "Test 2: Failed";
-        assert 0 == c.chop(1, new int[]{1}) : "Test 3: Failed";
-        assert 0 == c.chop(1, new int[]{1, 3, 5}) : "Test 4: Failed";
-        assert 1 == c.chop(3, new int[]{1, 3, 5}) : "Test 5: Failed";
-        assert 2 == c.chop(5, new int[]{1, 3, 5}) : "Test 6: Failed";
-        assert -1 == c.chop(0, new int[]{1, 3, 5}) : "Test 7: Failed";
-        assert -1 == c.chop(2, new int[]{1, 3, 5}) : "Test 8: Failed";
-        assert -1 == c.chop(4, new int[]{1, 3, 5}) : "Test 8: Failed";
-        assert -1 == c.chop(6, new int[]{1, 3, 5}) : "Test 10: Failed";
-        assert 0 == c.chop(1, new int[]{1, 3, 5, 7}) : "Test 11: Failed";
-        assert 1 == c.chop(3, new int[]{1, 3, 5, 7}) : "Test 12: Failed";
-        assert 2 == c.chop(5, new int[]{1, 3, 5, 7}) : "Test 13: Failed";
-        assert 3 == c.chop(7, new int[]{1, 3, 5, 7}) : "Test 14: Failed";
-        assert -1 == c.chop(0, new int[]{1, 3, 5, 7}) : "Test 15: Failed";
-        assert -1 == c.chop(2, new int[]{1, 3, 5, 7}) : "Test 16: Failed";
-        assert -1 == c.chop(4, new int[]{1, 3, 5, 7}) : "Test 17: Failed";
-        assert -1 == c.chop(6, new int[]{1, 3, 5, 7}) : "Test 18: Failed";
-        assert -1 == c.chop(8, new int[]{1, 3, 5, 7}) : "Test 19: Failed";
-        assert 13 == c.chop(100, new int[]{1, 3, 5, 7,9,11,33,44,55,66,77,88,99,100,101,999}) : "Test 20: Failed";
+    private int targetValue;
+    private int[] sortedArrayList;
+    private int expected;
+
+    public ChopTest(int targetValue, int[] sortedArrayList, int expected) {
+        this.targetValue = targetValue;
+        this.sortedArrayList = sortedArrayList;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {3,new int[]{},-1},
+                {3,new int[]{1},-1},
+                {1,new int[]{1},0},
+                {1,new int[]{1,3,5},0},
+                {3,new int[]{1,3,5},1},
+                {5,new int[]{1,3,5},2},
+                {0,new int[]{1,3,5},-1},
+                {2,new int[]{1,3,5},-1},
+                {4,new int[]{1,3,5},-1},
+                {6,new int[]{1,3,5},-1},
+                {1,new int[]{1,3,5,7},0},
+                {3,new int[]{1,3,5,7},1},
+                {5,new int[]{1,3,5,7},2},
+                {7,new int[]{1,3,5,7},3},
+                {0,new int[]{1,3,5,7},-1},
+                {2,new int[]{1,3,5,7},-1},
+                {4,new int[]{1,3,5,7},-1},
+                {6,new int[]{1,3,5,7},-1},
+                {8,new int[]{1,3,5,7},-1},
+                {100,new int[]{1, 3, 5, 7,9,11,33,44,55,66,77,88,99,100,101,999},13},
+        });
     }
     @Test
-    public void testChop2() {
+    public void testChop1(){
+        IChop c = new Chop1();
+        assertThat(c.chop(this.targetValue, this.sortedArrayList), is(this.expected));
+    }
+    @Test
+    public void testChop2(){
         IChop c = new Chop2();
-        assert -1 == c.chop(3, new int[]{}) : "Test 1: Failed";
-        assert -1 == c.chop(3, new int[]{1}) : "Test 2: Failed";
-        assert 0 == c.chop(1, new int[]{1}) : "Test 3: Failed";
-        assert 0 == c.chop(1, new int[]{1, 3, 5}) : "Test 4: Failed";
-        assert 1 == c.chop(3, new int[]{1, 3, 5}) : "Test 5: Failed";
-        assert 2 == c.chop(5, new int[]{1, 3, 5}) : "Test 6: Failed";
-        assert -1 == c.chop(0, new int[]{1, 3, 5}) : "Test 7: Failed";
-        assert -1 == c.chop(2, new int[]{1, 3, 5}) : "Test 8: Failed";
-        assert -1 == c.chop(4, new int[]{1, 3, 5}) : "Test 8: Failed";
-        assert -1 == c.chop(6, new int[]{1, 3, 5}) : "Test 10: Failed";
-        assert 0 == c.chop(1, new int[]{1, 3, 5, 7}) : "Test 11: Failed";
-        assert 1 == c.chop(3, new int[]{1, 3, 5, 7}) : "Test 12: Failed";
-        assert 2 == c.chop(5, new int[]{1, 3, 5, 7}) : "Test 13: Failed";
-        assert 3 == c.chop(7, new int[]{1, 3, 5, 7}) : "Test 14: Failed";
-        assert -1 == c.chop(0, new int[]{1, 3, 5, 7}) : "Test 15: Failed";
-        assert -1 == c.chop(2, new int[]{1, 3, 5, 7}) : "Test 16: Failed";
-        assert -1 == c.chop(4, new int[]{1, 3, 5, 7}) : "Test 17: Failed";
-        assert -1 == c.chop(6, new int[]{1, 3, 5, 7}) : "Test 18: Failed";
-        assert -1 == c.chop(8, new int[]{1, 3, 5, 7}) : "Test 19: Failed";
-        assert 13 == c.chop(100, new int[]{1, 3, 5, 7,9,11,33,44,55,66,77,88,99,100,101,999}) : "Test 20: Failed";
-
+        assertThat(c.chop(this.targetValue, this.sortedArrayList), is(this.expected));
     }
 
 }
